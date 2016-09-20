990de82ba9cff3e72d70538d8651481ac279d511
Andrei Richkov <huldoser@gmail.com>
2016-09-20 12:17:58 +0300

    removed static keywork because than  is not allowed

--- app/netekbot/config.class.php
+++ app/netekbot/config.class.php
@@ -2,21 +2,21 @@
 
   class config {
 
-    static $db = array(
+    $db = array(
       'dbName' => $ENV_['DB_NAME'],
       'dbUserName' => $ENV_['DB_USER_NAME'],
       'dbPassword' => $ENV_['DB_PASSWORD'],
       'dbUrl' => $ENV_['DB_URL']
     );
 
-    static $facebook = array(
+    $facebook = array(
       'fbAccessToken' => $ENV_['FB_ACCESS_TOKEN'],
       'fbVerificationToken' => $ENV_['FB_VERIFICATION_TOKEN'],
       'fbSendUrl' => 'https://graph.facebook.com/v2.7/me/messages?access_token='
 
     );
 
-    static $sendGrid = array(
+    $sendGrid = array(
       'sgUserName' => $ENV_['SG_USER_NAME'],
       'sgPassword' => $ENV_['SG_PASSWORD']
     );
