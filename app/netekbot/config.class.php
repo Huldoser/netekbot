0e812826fcd45f271c0df15e0acbb1ec7833f680
Andrei Richkov <huldoser@gmail.com>
2016-09-21 16:25:30 +0300

    used a workaround phps static expression limitation

--- app/netekbot/config.class.php
+++ app/netekbot/config.class.php
@@ -2,37 +2,49 @@
 
   class config {
 
-    private $database = array(
-      'dbName' => $_ENV['DB_NAME'],
-      'dbUserName' => $_ENV['DB_USER_NAME'],
-      'dbPassword' => $_ENV['DB_PASSWORD'],
-      'dbUrl' => $_ENV['DB_URL']
-    );
+    private static $array = null;
 
-    private $facebook = array(
-      'fbAccessToken' => $_ENV['FB_ACCESS_TOKEN'],
-      'fbVerificationToken' => $_ENV['FB_VERIFICATION_TOKEN'],
-      'fbSendUrl' => 'https://graph.facebook.com/v2.7/me/messages?access_token='
-    );
-
-    private $sendGrid = array(
-      'sgUserName' => $_ENV['SG_USER_NAME'],
-      'sgPassword' => $_ENV['SG_PASSWORD']
-    );
-    
-    // Blocking the abillity to make instances
+    // Blocking the abillity to make instances of this class
     private function __construct() {}
 
     public function getDatabase($key) {
-      self::$database[$key];
+      self::init('database');
+      return self::$array[$key];
     }
 
     public function getFacebook($key) {
-      self::$facebook[$key];
+      self::init('facebook');
+      return self::$array[$key];
     }
 
     public function getSendGrid($key) {
-      self::$sendGrid[$key];
+      self::init('sendGrid');
+      return self::$array[$key];
+    }
+
+    // Because static variable cannot accept expressions we need to init it
+    private function init($arrayName) {
+      if ($arrayName == 'database') {
+        self::$array = array(
+          'dbName' => $_ENV['DB_NAME'],
+          'dbUserName' => $_ENV['DB_USER_NAME'],
+          'dbPassword' => $_ENV['DB_PASSWORD'],
+          'dbUrl' => $_ENV['DB_URL']
+        );
+
+      } else if ($arrayName == 'facebook') {
+        self::$array = array(
+          'fbAccessToken' => $_ENV['FB_ACCESS_TOKEN'],
+          'fbVerificationToken' => $_ENV['FB_VERIFICATION_TOKEN'],
+          'fbSendUrl' => 'https://graph.facebook.com/v2.7/me/messages?access_token='
+        );
+
+      } else if ($arrayName == 'sendGrid') {
+        self::$array = array(
+          'sgUserName' => $_ENV['SG_USER_NAME'],
+          'sgPassword' => $_ENV['SG_PASSWORD']
+        );
+      }
     }
   }
 
